1e2247d63370cc23b0f5ac93e2aa1bc321b5d882
Alex Lamper <lamp0020@hz.nl>
2024-04-30 09:11:19 +0100

    Added Basic Navigation/Pages

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer" style="background-color: black; color: white">
+    <div class="content has-text-centered">
+        <p>
+            <strong style="color: #a0aec0">Scriptu</strong> by Alex Lamper. The source code is licensed under the WTFPL License.
+        </p>
+    </div>
+</footer>
